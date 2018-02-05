cf91aab8d971b077903948d3a0c20e1c17a57162
Anna Malysheva <anna_malysheva@epam.com>
2018-02-05 12:22:18 +0300

    Added moving in two directions

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-var frame,
+var frame1,
+    frame2,
     bounds,
     x,
     leftBound,
+    rightBound,
     minWidth;
 
 minWidth = 550;
 
-var clicked = {};
+var clicked;
 
-
-frame = document.getElementById('div2');
+frame1 = document.getElementById('div1');
+frame2 = document.getElementById('div2');
 
 document.addEventListener('mousedown', onMouseDown);
 document.addEventListener('mouseup', onMouseUp);
 
 function calculate(e) {
 
-    bounds = frame.getBoundingClientRect();
+    bounds = frame2.getBoundingClientRect();
     x = e.clientX - bounds.left;
     leftBound = x < 10 && x > 0;
+    rightBound = x < 0;
 
 }
 
@@ -26,18 +29,20 @@ function onMouseDown(e) {
 
     calculate(e);
 
+    console.log(clicked);
+
     clicked = {
         cx: e.clientX,
         x: x,
         w: bounds.width,
-        leftBound: leftBound
+        leftBound: leftBound,
+        rightBound: rightBound
     };
 
-    if (leftBound) {
-
-        frame.style.cursor = 'ew-resize';
+    if (0 < x < 10 || x < 0) {
+        frame2.style.cursor = 'ew-resize';
     } else {
-        frame.style.cursor = 'default';
+        frame2.style.cursor = 'default';
     }
 }
 
@@ -45,10 +50,33 @@ function onMouseUp(e) {
 
     calculate(e);
 
-    var currentWidth = Math.max(clicked.cx + clicked.w - e.clientX, minWidth);
-    if (currentWidth > minWidth && currentWidth > bounds.width) {
-        frame.style.width = currentWidth + 'px';
-        frame.style.left = e.clientX + 'px';
+    console.log(clicked);
+
+    if (clicked && (clicked.leftBound || clicked.rightBound)) {
+        var currentWidth;
+
+        if (clicked.leftBound) {
+
+            currentWidth = Math.max(clicked.cx + clicked.w - e.clientX, minWidth);
+            if (currentWidth > minWidth && currentWidth > bounds.width) {
+                frame2.style.width = currentWidth + 'px';
+                frame2.style.left = e.clientX + 'px';
+
+                frame1.style.width = e.clientX + 'px';
+            }
+
+        }
+
+        if (clicked.rightBound) {
+            currentWidth = Math.max(clicked.cx + clicked.w - e.clientX, minWidth);
+            if (currentWidth < bounds.width) {
+                frame1.style.width = e.clientX + 'px';
+
+                frame2.style.left = e.clientX + 'px';
+                frame2.style.width = currentWidth + 'px';
+            }
+        }
+
     }
 
 }
